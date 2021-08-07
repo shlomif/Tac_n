2f7839448633103063fc9ce0084f290de7f14457
Shlomi Fish <shlomif@shlomifish.org>
2021-08-07 10:07:26 +0300

    add tests

--- App-Tac_n/lib/App/Tac_n.pm
+++ App-Tac_n/lib/App/Tac_n.pm
@@ -4,3 +4,22 @@ use strict;
 use warnings;
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+App::Tac_n - tac with line numbers
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
+
